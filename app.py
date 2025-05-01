e9dbc2d52b7958fbc7c85ff27f6f501e86ca4025
SUDEM SONG DAIMARI <67313139+tsong44@users.noreply.github.com>
2025-05-01 14:44:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import os
 import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+# Disable GPU usage by setting CUDA_VISIBLE_DEVICES to ""
+os.environ["CUDA_VISIBLE_DEVICES"] = ""  # Disable GPU
+
+# Now import tensorflow after setting the environment variable
+import tensorflow as tf
+
 app = Flask(__name__)
 
 # Load model, tokenizer, and label encoder
@@ -54,9 +60,6 @@ def chat():
     return jsonify({"response": response})
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use the PORT Render provides
     app.run(debug=True, host="0.0.0.0", port=port)
-    
